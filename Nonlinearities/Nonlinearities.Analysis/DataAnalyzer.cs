50d2e52561ead559ec027ac68ba4c682afcf5979
robinsedlaczek <robin.sedlaczek@live.de>
2013-05-06 01:16:53 +0200

    - add library to handle MATLAB files - add unit tests

--- Nonlinearities/Nonlinearities.Analysis/DataAnalyzer.cs
+++ Nonlinearities/Nonlinearities.Analysis/DataAnalyzer.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using csmatio.io;
+using csmatio.types;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +10,20 @@ namespace Nonlinearities.Analysis
 {
     public class DataAnalyzer
     {
+        public DataAnalyzer()
+        {
+            var reader = new MatFileReader(@"F:\Development\Uni\Nonlinearities\Data\Long Experiment\msq1D.mat");
+
+            var stimuli = ((MLDouble)reader.GetMLArray("msq1D")).GetArray();
+
+            reader = new MatFileReader(@"F:\Development\Uni\Nonlinearities\Data\Long Experiment\spksV6.mat");
+
+            var spikes1 = ((MLDouble)reader.Data[0]).GetArray();
+            var spikes2 = ((MLDouble)reader.Data[1]).GetArray();
+            var spikes3 = ((MLDouble)reader.Data[2]).GetArray();
+            var spikes4 = ((MLDouble)reader.Data[3]).GetArray();
+            
+        }
 
     }
 }
---
+++ Nonlinearities/Nonlinearities.Tests/DataAnalyzerTests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Nonlinearities.Analysis;
+
+namespace Nonlinearities.Tests
+{
+    [TestClass]
+    public class DataAnalyzerTests
+    {
+        [TestMethod]
+        public void TestOpenFile()
+        {
+            var analyzer = new DataAnalyzer();
+
+        }
+    }
+}
